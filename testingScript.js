79018978f697f8ce4c225f1dcd2825dae1f2096d
Amrit Raj <73699121+AmritRajCoder@users.noreply.github.com>
2021-01-06 12:54:55 +0530

    Add files via upload

---
+++ testingScript.js
@@ -0,0 +1,77 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBo7BB65wqw7XAhxaHscBR415HLD9Yhyfc",
+    authDomain: "corona-testing-1ff42.firebaseapp.com",
+    databaseURL: "https://corona-testing-1ff42.firebaseio.com",
+    projectId: "corona-testing-1ff42",
+    storageBucket: "corona-testing-1ff42.appspot.com",
+    messagingSenderId: "970747869393",
+    appId: "1:970747869393:web:6bc255ce6aac903845edf0"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var UserInputsRef=firebase.database().ref('UserInputs')
+document.getElementById('testForm').addEventListener('submit', submitForm);
+function submitForm(e){
+    e.preventDefault();
+    var fname = getInputVal('firstname');
+    var lname = getInputVal('lastname');
+    var mobile = getInputVal('mobile');
+    var state = getInputVal('state');
+    state = state.toLowerCase();
+    readState(state);
+    var email = getInputVal('email');
+    var emailstatus = validateEmail();
+    var profession = getInputVal('profession');
+    var dateofbirth = getInputVal('dateofbirth');
+    var symptomsList = getSelectedCheckboxValues('symptoms');
+    var selectedOption = document.querySelector('input[name = option:checked').value;
+    if (emailstatus){
+    saveMessages(fname + " " + lname, mobile, email, profession, dateofbirth, state, selectedOption, symptomsList);}
+}
+
+function readState(state){
+    var centers;
+    var ref = firebase.database().ref(state);
+    ref.on('value', (data) => {
+        centers = data.val();
+        document.getElementById("result").innerHTML = "<br>" + centers.toUpperCase();
+    })
+}
+
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
+
+function saveMessages(name, mobile, email, profession, dateofbirth, state, selectedOption, symptomsList){
+    var newuserInputsRef = UserInputsRef.push();
+    newuserInputsRef.set({
+        name: name,
+        mobile: mobile,
+        email: email,
+        profession: profession,
+        dateofbirth: dateofbirth,
+        selectedOption: selectedOption,
+        state: state,
+        symptomsList: symptomsList
+    })
+
+    alert("Thank you, find the list of centers nearby!");
+}
+
+function getSelectedCheckboxValues(name){
+    const checkboxes = document.querySelectorAll(`input[name = "${name}"]:checked`);
+    let values = [];
+    checkboxes.forEach((checkbox) => {
+        values.push(checkbox.value);
+    });
+
+    return values;
+}
+function validateEmail(){
+    if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(testForm.email.value)){
+        return(true)
+    }
+    alert("You have  entered  an invalid email address!")
+    return(false)
+}
